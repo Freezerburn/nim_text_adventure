44020cfe835d577b849e48c16673d81a39103efc
Vincent K <FreezerburnV@Vincents-Air.home>
2014-10-19 23:28:41 -0400

    Start moving state handling to separate thread.

--- main.nim
+++ main.nim
@@ -7,7 +7,11 @@ import actors
 import os
 
 const
-    NUM_THREADS = 3
+    NUM_THREADS = 4
+    PLAYER_THREAD_INDEX = 0
+    TICK_THREAD_INDEX = 1
+    LOG_THREAD_INDEX = 2
+    STATE_THREAD_INDEX = 3
 
     startRoomName = "StartRoom"
     startRoomDescription = "A simple room."
@@ -15,21 +19,64 @@ const
     eastRoomName = "EastRoom"
     eastRoomDescription = "A room to the east of a simple room."
 
+    playerName = "ThePlayer"
+    playerDescription = "The player of this game."
+    playerStart = startRoomName
+
+    shopkeepName = "Shopkeeper"
+    shopkeepDescription = "A wandering shopkeeper. Maybe you can buy something from him?"
+    shopkeepStart = eastRoomName
+
 type
     Direction = enum
         DirNorth, DirSouth, DirEast, DirWest,
         DirUp, DirDown,
         DirOut, DirIn
 
+    LogLevel = enum
+        LevelDebug,
+        LevelInfo,
+        LevelWarning,
+        LevelError,
+        LevelCritical,
+        LevelNone
     MessageKind = enum
-        MessageQuit,
-        MessageLog
+        MessageAck,
+
+        MessageLog,
+
+        # State thread messages.
+        MoveEntity,
+        PrintRoom,
+        PrintRoomAtEnt,
+        DescribeRoom,
+        DescribeRoomAtEnt,
+        DescribeEnt,
+
+        MessageQuit
     Message = object
+        ackChan: int
         case kind: MessageKind
         of MessageLog:
             msg: string
             newline: bool
+            level: LogLevel
+        of MoveEntity:
+            meEnt: string
+            meDir: Direction
+        of PrintRoom:
+            prLoc: string
+        of PrintRoomAtEnt:
+            pratEnt: string
+        of DescribeRoom:
+            drLoc: string
+        of DescribeRoomAtEnt:
+            dratEnt: string
+        of DescribeEnt:
+            deEnt: string
+            deLoc: string
         of MessageQuit: nil
+        else: nil
 
     PRoom = ref Room
     Room = object of TObject
@@ -37,6 +84,14 @@ type
         description*: string
         directions: array[DirNorth..DirIn, PRoom]
         linked: TSet[PRoom]
+        ents: TDoublyLinkedList[PEntity]
+
+    PEntity = ref Entity
+    Entity = object of TObject
+        name: string
+        description: string
+        id: int
+        location: string
 
     OptionKind = enum
         SomeKind, NoneKind
@@ -48,11 +103,15 @@ type
             nil
     EOptionFail = object of E_Base
 
-    ThreadArg = array[NUM_THREADS, ptr TChannel[Message]]
+    ChannelType = TChannel[Message]
+    PChannelType = ptr ChannelType
+    ThreadArg = array[NUM_THREADS, PChannelType]
+    GameThread = TTHread[ThreadArg]
 
 template playerChan: expr = channels[0][]
 template tickChan: expr = channels[1][]
 template logChan: expr = channels[2][]
+template stateChan: expr = channels[3][]
 
 proc newRoom(name, description: string): PRoom
 
@@ -72,11 +131,41 @@ proc unwrap[T](opt: Option[T]): T =
     of SomeKind: return opt.value
     of NoneKind: raise newException(EOptionFail, "Tried to unwrap a none.")
 
-proc log(msg: string): Message =
-    return Message(kind: MessageLog, msg: msg, newline: false)
+template log(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: false, level: LevelNone))
 
-proc logLine(msg: string): Message =
-    return Message(kind: MessageLog, msg: msg, newline: true)
+template logLine(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: true, level: LevelNone))
+
+template logDebug(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: false, level: LevelDebug))
+
+template logDebugLine(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: true, level: LevelDebug))
+
+template logInfo(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: false, level: LevelInfo))
+
+template logInfoLine(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: true, level: LevelInfo))
+
+template logWarning(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: false, level: LevelWarning))
+
+template logWarningLine(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: true, level: LevelWarning))
+
+template logError(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: false, level: LevelError))
+
+template logErrorLine(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: true, level: LevelError))
+
+template logCritical(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: false, level: LevelCritical))
+
+template logCriticalLine(msgStr: string) =
+    logChan.send(Message(ackChan: NUM_THREADS, kind: MessageLog, msg: msgStr, newline: true, level: LevelCritical))
 
 proc threadQuit(): Message =
     return Message(kind: MessageQuit)
@@ -115,6 +204,9 @@ proc clone(room: PRoom): PRoom =
     result.directions = room.directions
     result.linked = room.linked
 
+proc `$`(ent: PEntity): string =
+    return $(ent[])
+
 proc `$`(dirArr: array[DirNorth..DirIn, PRoom]): string =
     return repr(dirArr)
 
@@ -141,6 +233,7 @@ proc newRoom(name, description: string): PRoom =
     result.name = name
     result.description = description
     result.linked = initSet[PRoom]()
+    result.ents = initDoublyLinkedList[PEntity]()
 
 proc linkRooms(first, second: PRoom, directionToSecond: Direction) =
     first.directions[directionToSecond] = second
@@ -164,12 +257,37 @@ proc initRooms(): TTable[string, PRoom] =
     linkRooms(startRoom, eastRoom, DirEast)
     linkRooms(startRoom, southRoom, DirSouth)
 
+proc initEntities(rooms: var TTable[string, PRoom]): TTable[string, PEntity] =
+    result = initTable[string, PEntity]()
+    var
+        player: PEntity
+        shopkeep: PEntity
+
+    new(player)
+    player.name = playerName
+    player.description = playerDescription
+    player.location = playerStart
+    result[player.name] = player
+
+    new(shopkeep)
+    shopkeep.name = shopkeepName
+    shopkeep.description = shopkeepDescription
+    shopkeep.location = shopkeepStart
+    result[shopkeep.name] = shopkeep
+
+    rooms[player.location].ents.append(player)
+    rooms[shopkeep.location].ents.append(shopkeep)
+
 proc logThreadFunc(channels: ThreadArg) {.thread.} =
     var going = true
     while going:
         let msg = logChan.recv()
         case msg.kind:
         of MessageLog:
+            let pre = case msg.level
+                of LevelDebug: "[DEBUG] "
+                else: ""
+            stdout.write(pre)
             if msg.newline:
                 echo(msg.msg)
             else:
@@ -177,7 +295,40 @@ proc logThreadFunc(channels: ThreadArg) {.thread.} =
                 stdout.flushFile()
         of MessageQuit:
             going = false
-        sleep(20)
+        else:
+            discard
+
+        if msg.ackChan < NUM_THREADS:
+            channels[msg.ackChan][].send(Message(kind: MessageAck))
+
+proc stateThreadFunc(channels: ThreadArg) {.thread.} =
+    var rooms = initRooms()
+    var ents = initEntities(rooms)
+
+    var going = true
+    while going:
+        let msg = stateChan.recv()
+        echo($msg)
+        case msg.kind:
+        of PrintRoomAtEnt:
+            if ents.hasKey(msg.pratEnt):
+                let ent = ents[msg.pratEnt]
+                let room = rooms[ent.location]
+                log("Exits are: ")
+                var directionsToWrite = ""
+                for direction in low(room.directions)..high(room.directions):
+                    if room.directions[direction] != nil:
+                        directionsToWrite.add(($direction)[3.. -1] & ", ")
+                logLine(directionsToWrite[0.. -3])
+            else:
+                logWarningLine("Could not find the given entity: " & msg.pratEnt)
+        of MessageQuit:
+            going = false
+        else:
+            discard
+
+        if msg.ackChan < NUM_THREADS:
+            channels[msg.ackChan][].send(Message(kind: MessageAck))
 
 proc tickThreadFunc(channels: ThreadArg) {.thread.} =
     var going = true
@@ -190,24 +341,17 @@ proc tickThreadFunc(channels: ThreadArg) {.thread.} =
             discard
 
 proc playerThreadFunc(channels: ThreadArg) {.thread.} =
-    let rooms = initRooms()
-    var currentRoom = rooms[startRoomName]
-
     var going = true
     var printDescription = true
     while going:
-        if printDescription:
-            printDescription = false
-            echo(currentRoom.description)
+        # if printDescription:
+        #     printDescription = false
+        #     echo(currentRoom.description)
 
-        logChan.send(log("Exits are: "))
-        var directionsToWrite = ""
-        for direction in low(currentRoom.directions)..high(currentRoom.directions):
-            if currentRoom.directions[direction] != nil:
-                directionsToWrite.add(($direction)[3.. -1] & ", ")
-        logChan.send(logLine(directionsToWrite[0.. -3]))
+        stateChan.send(Message(ackChan: PLAYER_THREAD_INDEX, kind: PrintRoomAtEnt, pratEnt: playerName))
+        discard stateChan.recv()
 
-        logChan.send(log(">>>> "))
+        log(">>>>  ")
         let line = readLine(stdin).strip()
         let toProcess = line.split(" ")
 
@@ -220,43 +364,49 @@ proc playerThreadFunc(channels: ThreadArg) {.thread.} =
                     let maybeDir = toProcess[1].toDirection()
                     if maybeDir.isSome():
                         let direction: Direction = unwrap(maybeDir)
-                        if currentRoom.directions[direction] != nil:
-                            let room = currentRoom.directions[direction]
-                            currentRoom = room
-                            printDescription = true
-                        else:
-                            logChan.send(logLine("There are no exist in that direction."))
+                        # if currentRoom.directions[direction] != nil:
+                        #     let room = currentRoom.directions[direction]
+                        #     currentRoom = room
+                        #     printDescription = true
+                        # else:
+                        #     logLine("There are no exits in that direction.")
             else:
-                logChan.send(logLine("I do not understand that command."))
+                logLine("I do not understand that command.")
 
         if line == "exit":
             going = false
 
     tickChan.send(Message(kind: MessageQuit))
     logChan.send(Message(kind: MessageQuit))
+    stateChan.send(threadQuit())
 
 when isMainModule:
     var
         actualPlayerChan: TChannel[Message]
         actualTickChan: TChannel[Message]
         actualLogChan: TChannel[Message]
+        actualStateChan: TChannel[Message]
 
         channels: array[NUM_THREADS, ptr TChannel[Message]]
 
         playerThread: TThread[array[NUM_THREADS, ptr TChannel[Message]]]
         tickThread: TTHread[array[NUM_THREADS, ptr TChannel[Message]]]
         logThread: TTHread[array[NUM_THREADS, ptr TChannel[Message]]]
+        stateThread: GameThread
 
     open(actualPlayerChan)
     open(actualTickChan)
     open(actualLogChan)
+    open(actualStateChan)
 
     channels[0] = addr(actualPlayerChan)
     channels[1] = addr(actualTickChan)
     channels[2] = addr(actualLogChan)
+    channels[3] = addr(actualStateChan)
 
     createThread(playerThread, playerThreadFunc, channels)
     createThread(tickThread, tickThreadFunc, channels)
     createThread(logThread, logThreadFunc, channels)
+    createThread(stateThread, stateThreadFunc, channels)
 
-    joinThreads(playerThread, tickThread, logThread)
+    joinThreads(playerThread, tickThread, logThread, stateThread)
